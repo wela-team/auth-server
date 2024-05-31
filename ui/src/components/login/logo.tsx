1b3eea4595b937c4441d64e2312967f1a458c058
Elman Torres <63641794+wela-elman@users.noreply.github.com>
2024-05-31 10:43:57 +0800

    Update logo.tsx

--- ui/src/components/login/logo.tsx
+++ ui/src/components/login/logo.tsx
@@ -2,7 +2,7 @@
 export default function Logo() {
   return (
     <div className="logo m-2">
-      <img alt="Papermerge logo" width="112" height="108" src="/images/papermerge4.svg" />
+      <img alt="Wela logo" width="112" height="108" src="/images/WELA-NEW-LOGO-01.svg" />
     </div>
   );
 }
